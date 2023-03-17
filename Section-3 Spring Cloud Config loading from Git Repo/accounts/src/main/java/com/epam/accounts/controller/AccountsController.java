12377cf5c6538ebf7e04343180c2e68f02e432a8
Amit Sharma1 <amit_sharma1@epam.com>
2023-03-17 18:22:58 -0400

    adding-cloud-config-changes

--- Section-3 Spring Cloud Config loading from Git Repo/accounts/src/main/java/com/epam/accounts/controller/AccountsController.java
+++ Section-3 Spring Cloud Config loading from Git Repo/accounts/src/main/java/com/epam/accounts/controller/AccountsController.java
@@ -2,13 +2,19 @@ package com.epam.accounts.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.epam.accounts.config.AccountServiceConfig;
 import com.epam.accounts.model.Accounts;
 import com.epam.accounts.model.Customer;
+import com.epam.accounts.model.Properties;
 import com.epam.accounts.repository.AccountsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 
 @RestController
@@ -16,6 +22,9 @@ public class AccountsController {
 	
 	@Autowired
 	private AccountsRepository accountsRepository;
+	
+	@Autowired
+	private AccountServiceConfig accountServiceConfig;
 
 	@PostMapping("/myAccount")
 	public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -27,6 +36,16 @@ public class AccountsController {
 			return null;
 		}
 
+	}
+	
+	@GetMapping("/accountDetails")
+	public String getAccountDetails() throws JsonProcessingException {
+		ObjectWriter writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(accountServiceConfig.getMsg(),accountServiceConfig.getBuildVersion()
+				,accountServiceConfig.getMailDetails(),accountServiceConfig.getActiveBranches());
+		String accountData = writer.writeValueAsString(properties);
+		return accountData;
+		
 	}
 
 }
--- Section-3 Spring Cloud Config loading from Git Repo/loans/src/main/java/com/epam/loans/controller/LoansController.java
+++ Section-3 Spring Cloud Config loading from Git Repo/loans/src/main/java/com/epam/loans/controller/LoansController.java
@@ -4,19 +4,28 @@ package com.epam.loans.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.epam.loans.config.LoansConfigService;
 import com.epam.loans.model.Customer;
 import com.epam.loans.model.Loans;
+import com.epam.loans.model.Properties;
 import com.epam.loans.repository.LoansRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 @RestController
 public class LoansController {
 
 	@Autowired
 	private LoansRepository loansRepository;
+	
+	@Autowired
+	private LoansConfigService loansConfigService;
 
 	@PostMapping("/myLoans")
 	public List<Loans> getLoansDetails(@RequestBody Customer customer) {
@@ -29,4 +38,13 @@ public class LoansController {
 
 	}
 
+	@GetMapping("/loansData")
+	public String getLoansConfigData() throws JsonProcessingException {
+		ObjectWriter writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(loansConfigService.getMsg(),loansConfigService.getBuildVersion(),
+			loansConfigService.getMailDetails(),loansConfigService.getActiveBranches());
+		String loansData = writer.writeValueAsString(properties);
+		return loansData;
+	}
+	
 }
