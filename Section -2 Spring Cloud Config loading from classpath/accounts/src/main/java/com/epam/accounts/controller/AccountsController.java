b91c4afaa8be6dbf7e16e93fdf39a3b428abacbb
Amit Sharma1 <amit_sharma1@epam.com>
2023-03-17 18:56:11 -0400

    adding

--- Section -2 Spring Cloud Config loading from classpath/accounts/src/main/java/com/epam/accounts/controller/AccountsController.java
+++ Section -2 Spring Cloud Config loading from classpath/accounts/src/main/java/com/epam/accounts/controller/AccountsController.java
@@ -2,20 +2,13 @@ package com.epam.accounts.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.epam.accounts.config.AccountsServiceConfig;
 import com.epam.accounts.model.Accounts;
 import com.epam.accounts.model.Customer;
-import com.epam.accounts.model.Properties;
 import com.epam.accounts.repository.AccountsRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-
 
 
 @RestController
@@ -23,9 +16,6 @@ public class AccountsController {
 	
 	@Autowired
 	private AccountsRepository accountsRepository;
-	
-	@Autowired
-	private AccountsServiceConfig accountsConfig;
 
 	@PostMapping("/myAccount")
 	public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -38,5 +28,5 @@ public class AccountsController {
 		}
 
 	}
-	
+
 }
