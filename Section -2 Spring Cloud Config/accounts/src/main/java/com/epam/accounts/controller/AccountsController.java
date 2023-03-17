7bbbca16bc387deca8679b12fe377974e30258b4
Amit Sharma1 <amit_sharma1@epam.com>
2023-03-17 12:33:22 -0400

    committing-section-2-spring-cloud-config

---
+++ Section -2 Spring Cloud Config/accounts/src/main/java/com/epam/accounts/controller/AccountsController.java
@@ -0,0 +1,32 @@
+package com.epam.accounts.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.epam.accounts.model.Accounts;
+import com.epam.accounts.model.Customer;
+import com.epam.accounts.repository.AccountsRepository;
+
+
+@RestController
+public class AccountsController {
+	
+	@Autowired
+	private AccountsRepository accountsRepository;
+
+	@PostMapping("/myAccount")
+	public Accounts getAccountDetails(@RequestBody Customer customer) {
+
+		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+		if (accounts != null) {
+			return accounts;
+		} else {
+			return null;
+		}
+
+	}
+
+}
