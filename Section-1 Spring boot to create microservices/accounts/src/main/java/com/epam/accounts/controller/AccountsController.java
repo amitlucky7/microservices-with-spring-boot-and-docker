f379d6f4d0854365969025feb607e8aa5ba1b095
Amit Sharma1 <amit_sharma1@epam.com>
2023-03-17 18:50:15 -0400

    deleting-files

--- Section-1 Spring boot to create microservices/accounts/src/main/java/com/epam/accounts/controller/AccountsController.java
+++ Section-1 Spring boot to create microservices/accounts/src/main/java/com/epam/accounts/controller/AccountsController.java
@@ -2,13 +2,20 @@ package com.epam.accounts.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.epam.accounts.config.AccountsServiceConfig;
 import com.epam.accounts.model.Accounts;
 import com.epam.accounts.model.Customer;
+import com.epam.accounts.model.Properties;
 import com.epam.accounts.repository.AccountsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
 
 
 @RestController
@@ -16,6 +23,9 @@ public class AccountsController {
 	
 	@Autowired
 	private AccountsRepository accountsRepository;
+	
+	@Autowired
+	private AccountsServiceConfig accountsConfig;
 
 	@PostMapping("/myAccount")
 	public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -27,6 +37,15 @@ public class AccountsController {
 			return null;
 		}
 
+	}
+	
+	@GetMapping("/account/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(accountsConfig.getMsg(), accountsConfig.getBuildVersion(),
+				accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
+		String jsonStr = ow.writeValueAsString(properties);
+		return jsonStr;
 	}
 
 }
