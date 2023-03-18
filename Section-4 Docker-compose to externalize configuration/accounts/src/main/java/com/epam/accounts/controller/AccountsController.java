636ab93fea0ff0b4120c72a76a8272dbd74bb442
Amit Sharma1 <amit_sharma1@epam.com>
2023-03-18 15:06:11 -0400

    changes-to-docker-compose

---
+++ Section-4 Docker-compose to externalize configuration/accounts/src/main/java/com/epam/accounts/controller/AccountsController.java
@@ -0,0 +1,51 @@
+package com.epam.accounts.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.epam.accounts.config.AccountServiceConfig;
+import com.epam.accounts.model.Accounts;
+import com.epam.accounts.model.Customer;
+import com.epam.accounts.model.Properties;
+import com.epam.accounts.repository.AccountsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+
+@RestController
+public class AccountsController {
+	
+	@Autowired
+	private AccountsRepository accountsRepository;
+	
+	@Autowired
+	private AccountServiceConfig accountServiceConfig;
+
+	@PostMapping("/myAccount")
+	public Accounts getAccountDetails(@RequestBody Customer customer) {
+
+		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+		if (accounts != null) {
+			return accounts;
+		} else {
+			return null;
+		}
+
+	}
+	
+	@GetMapping("/accounts/properties")
+	public String getAccountDetails() throws JsonProcessingException {
+		ObjectWriter writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(accountServiceConfig.getMsg(),accountServiceConfig.getBuildVersion()
+				,accountServiceConfig.getMailDetails(),accountServiceConfig.getActiveBranches());
+		String accountData = writer.writeValueAsString(properties);
+		return accountData;
+		
+	}
+
+}
---
+++ Section-4 Docker-compose to externalize configuration/loans/src/main/java/com/epam/loans/controller/LoansController.java
@@ -0,0 +1,50 @@
+package com.epam.loans.controller;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.epam.loans.config.LoansConfigService;
+import com.epam.loans.model.Customer;
+import com.epam.loans.model.Loans;
+import com.epam.loans.model.Properties;
+import com.epam.loans.repository.LoansRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+@RestController
+public class LoansController {
+
+	@Autowired
+	private LoansRepository loansRepository;
+	
+	@Autowired
+	private LoansConfigService loansConfigService;
+
+	@PostMapping("/myLoans")
+	public List<Loans> getLoansDetails(@RequestBody Customer customer) {
+		List<Loans> loans = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+		if (loans != null) {
+			return loans;
+		} else {
+			return null;
+		}
+
+	}
+
+	@GetMapping("/loans/properties")
+	public String getLoansConfigData() throws JsonProcessingException {
+		ObjectWriter writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(loansConfigService.getMsg(),loansConfigService.getBuildVersion(),
+			loansConfigService.getMailDetails(),loansConfigService.getActiveBranches());
+		String loansData = writer.writeValueAsString(properties);
+		return loansData;
+	}
+	
+}
